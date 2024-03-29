7cd22ce895eb70cd834ed65bdcb079569187c111
Jurgen Stegeman <jurgenstegeman@live.nl>
2024-03-29 14:17:45 +0100

    Implement auth blueprint

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_migrate import Migrate
 from flask_login import (
-    login_user,
     login_required,
-    logout_user,
     LoginManager,
     current_user,
 )
 from forms.register_form import RegisterForm
-from forms.login import LoginForm
 from forms.new_list import NewListForm
 from forms.add_task import AddTaskForm
 from forms.edit_tasks import EditTaskForm
@@ -17,8 +14,7 @@ from dotenv import load_dotenv
 import os
 from datetime import datetime
 from database.tables import db, Users, Lists, Groups, GroupMembers, Tasks, GroupInvites
-from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.exc import IntegrityError
+from auth.views import auth
 
 load_dotenv()
 
@@ -37,20 +33,14 @@ migrate = Migrate(app, db)
 # Flask_login stuff
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = "auth.login"
+
+app.register_blueprint(auth, url_prefix="/auth")
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return db.session.get(Users, int(user_id))
-
-
-@app.route("/account", methods=["GET", "POST"])
-@login_required
-def account():
-    active_page = "account"
-
-    return render_template("account.html", active_page=active_page)
 
 
 @app.route("/invites/<int:list_id>/accept")
@@ -141,25 +131,6 @@ def add_task(list_id):
         group_members_ids=group_members_ids,
         list_id=list_id,
     )
-
-
-@app.route("/delete/<int:id>", methods=["GET", "POST"])
-def delete_account(id):
-    user_to_delete = Users.query.get_or_404(id)
-
-    if current_user.id == user_to_delete.id:
-        try:
-            db.session.delete(user_to_delete)
-            db.session.commit()
-            flash("User Deleted Successfully!!")
-
-            return render_template("delete.html")
-        except:
-            flash("Error! Looks like there was a problem.... Try Again!")
-            return render_template("delete.html")
-    else:
-        flash("Please Don't Try To Delete Other Peoples Account. That Is Not Nice!")
-        return render_template("delete.html")
 
 
 @app.route("/lists/delete_list/<int:list_id>", methods=["GET", "POST"])
@@ -324,32 +295,6 @@ def edit_task(list_id, task_id):
     )
 
 
-@app.route("/edit_user/<int:id>", methods=["GET", "POST"])
-@login_required
-def edit_user(id):
-    form = RegisterForm()
-    user_to_update = Users.query.get_or_404(id)
-    if request.method == "POST":
-        user_to_update.first_name = request.form["first_name"]
-        user_to_update.last_name = request.form["last_name"]
-        user_to_update.email = request.form["email"]
-        try:
-            db.session.commit()
-            flash("User Updated Successfully!")
-            return render_template(
-                "edit_user.html", form=form, user_to_update=user_to_update, id=id
-            )
-        except:
-            flash("Error! Looks like there was a problem.... Try Again!")
-            return render_template(
-                "edit_user.html", form=form, user_to_update=user_to_update
-            )
-    else:
-        return render_template(
-            "edit_user.html", form=form, user_to_update=user_to_update, id=id
-        )
-
-
 @app.route("/invites")
 @login_required
 def group_invites():
@@ -429,35 +374,6 @@ def leave_group(list_id):
         )
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    active_page = "login"
-
-    if form.validate_on_submit():
-        # Lookup user
-        user = Users.query.filter_by(username=form.username.data).first()
-        if user:
-            # Check the hash
-            if check_password_hash(user.password_hash, form.password.data):
-                login_user(user)
-                return redirect(url_for("account"))
-            else:
-                flash("Wrong Password - Try Again!")
-        else:
-            flash("That Username Doesn't Exist! Try Again!")
-
-    return render_template("login.html", form=form, active_page=active_page)
-
-
-@app.route("/logout", methods=["GET", "POST"])
-@login_required
-def logout():
-    logout_user()
-    flash("You Logged Out!")
-    return redirect(url_for("login"))
-
-
 @app.route("/lists/new_list", methods=["GET", "POST"])
 @login_required
 def new_list():
@@ -500,56 +416,6 @@ def personal_lists():
     return render_template(
         "personal_lists.html",
         lists_with_active_tasks=lists_with_active_tasks,
-    )
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    first_name = None
-    last_name = None
-    email = None
-    form = RegisterForm()
-    active_page = "register"
-
-    # Validate form
-    if form.validate_on_submit():
-        user = Users.query.filter_by(email=form.email.data).first()
-        if user is None:
-            full_name = form.first_name.data + " " + form.last_name.data
-            # Hash password
-            hashed_pw = generate_password_hash(
-                form.password_hash.data, method="pbkdf2:sha256"
-            )
-
-            user = Users(
-                first_name=form.first_name.data,
-                last_name=form.last_name.data,
-                username=form.username.data,
-                full_name=full_name,
-                email=form.email.data,
-                password_hash=hashed_pw,
-            )
-            db.session.add(user)
-            db.session.commit()
-            flash("Successfully registered")
-
-            form.first_name.data = ""
-            form.last_name.data = ""
-            form.username.data = ""
-            form.email.data = ""
-            form.password_hash.data = ""
-        else:
-            flash("Email adress already in use")
-
-    our_users = Users.query.order_by(Users.date_added)
-    return render_template(
-        "register.html",
-        first_name=first_name,
-        last_name=last_name,
-        email=email,
-        form=form,
-        our_users=our_users,
-        active_page=active_page,
     )
 
 
---
+++ auth/views.py
@@ -0,0 +1,138 @@
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from database.tables import db, Users
+from forms.register_form import RegisterForm
+from forms.login import LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint("auth", __name__, template_folder="templates")
+
+
+@auth.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    active_page = "account"
+
+    return render_template("account.html", active_page=active_page)
+
+
+@auth.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete_account(id):
+    user_to_delete = Users.query.get_or_404(id)
+
+    if current_user.id == user_to_delete.id:
+        try:
+            db.session.delete(user_to_delete)
+            db.session.commit()
+            flash("User Deleted Successfully!!")
+
+            return render_template("delete.html")
+        except:
+            flash("Error! Looks like there was a problem.... Try Again!")
+            return render_template("delete.html")
+    else:
+        flash("Please Don't Try To Delete Other Peoples Account. That Is Not Nice!")
+        return render_template("delete.html")
+
+
+@auth.route("/edit_user/<int:id>", methods=["GET", "POST"])
+@login_required
+def edit_user(id):
+    form = RegisterForm()
+    user_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        user_to_update.first_name = request.form["first_name"]
+        user_to_update.last_name = request.form["last_name"]
+        user_to_update.email = request.form["email"]
+        try:
+            db.session.commit()
+            flash("User Updated Successfully!")
+            return render_template(
+                "edit_user.html", form=form, user_to_update=user_to_update, id=id
+            )
+        except:
+            flash("Error! Looks like there was a problem.... Try Again!")
+            return render_template(
+                "edit_user.html", form=form, user_to_update=user_to_update
+            )
+    else:
+        return render_template(
+            "edit_user.html", form=form, user_to_update=user_to_update, id=id
+        )
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    active_page = "login"
+
+    if form.validate_on_submit():
+        # Lookup user
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            # Check the hash
+            if check_password_hash(user.password_hash, form.password.data):
+                login_user(user)
+                return redirect(url_for("auth.account"))
+            else:
+                flash("Wrong Password - Try Again!")
+        else:
+            flash("That Username Doesn't Exist! Try Again!")
+
+    return render_template("login.html", form=form, active_page=active_page)
+
+
+@auth.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    flash("You Logged Out!")
+    return redirect(url_for("auth.login"))
+
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    first_name = None
+    last_name = None
+    email = None
+    form = RegisterForm()
+    active_page = "register"
+
+    # Validate form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            full_name = form.first_name.data + " " + form.last_name.data
+            # Hash password
+            hashed_pw = generate_password_hash(
+                form.password_hash.data, method="pbkdf2:sha256"
+            )
+
+            user = Users(
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                username=form.username.data,
+                full_name=full_name,
+                email=form.email.data,
+                password_hash=hashed_pw,
+            )
+            db.session.add(user)
+            db.session.commit()
+            flash("Successfully registered")
+
+            form.first_name.data = ""
+            form.last_name.data = ""
+            form.username.data = ""
+            form.email.data = ""
+            form.password_hash.data = ""
+        else:
+            flash("Email adress already in use")
+
+    return render_template(
+        "register.html",
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        form=form,
+        active_page=active_page,
+    )
